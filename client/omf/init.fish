1d7d6d54162f1773f0de850bc836a1cae212a28c
Jay Zawrotny <jayzawrotny@gmail.com>
2019-06-13 21:44:19 -0400

    Updated tmux config and omf/init.fish

--- client/omf/init.fish
+++ client/omf/init.fish
@@ -1,11 +1,22 @@
 # BEHAVIOR CHANGES
 ##############################################################################
-fish_vi_key_bindings
+# fish_vi_key_bindings
+# fish_hybrid_key_bindings
+# fish_default_key_bindings
 
 # PATH
 ##############################################################################
-set -x PATH $HOME/bin $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin $PATH 
+set -x PATH $HOME/.npm-global/bin $HOME/bin $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin $PATH
 
 
 # CONFIG
 ##############################################################################
+set -gx JAVA_TOOL_OPTIONS "--add-modules=java.xml.bind"
+
+# Conditional Configs
+##############################################################################
+if test "$TERM" = "dumb"
+    fish_default_key_bindings
+else
+    fish_hybrid_key_bindings
+end
