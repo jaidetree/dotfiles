550e40952facf7a09069310b095872f94258b9a9
Jay <jayzawrotny@gmail.com>
2020-06-07 14:58:10 -0400

    Added window restore back, it's not perfect but better than nothing

--- client/doom.d/config.el
+++ client/doom.d/config.el
@@ -131,6 +131,26 @@
 (setq initial-frame-alist
   '((fullscreen . maximized)))
 
+;; add to ~/.doom.d/config.el
+(when-let (dims (doom-store-get 'last-frame-size))
+  (cl-destructuring-bind ((left . top) width height fullscreen) dims
+    (setq initial-frame-alist
+          (append initial-frame-alist
+                  `((left . ,left)
+                    (top . ,top)
+                    (width . ,width)
+                    (height . ,height)
+                    (fullscreen . ,fullscreen))))))
+
+(defun save-frame-dimensions ()
+  (doom-store-put 'last-frame-size
+                  (list (frame-position)
+                        (frame-width)
+                        (frame-height)
+                        (frame-parameter nil 'fullscreen))))
+
+(add-hook 'kill-emacs-hook #'save-frame-dimensions)
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; JavaScript
