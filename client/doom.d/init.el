9ae0780743207f3e43dd67b2b40522460e6cef0e
Jay Zawrotny <jayzawrotny@gmail.com>
2020-06-10 16:31:08 -0400

    Prepare to dive into org mode

--- client/doom.d/init.el
+++ client/doom.d/init.el
@@ -142,7 +142,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +brain +roam)  ; organize your plain life in plain text
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
