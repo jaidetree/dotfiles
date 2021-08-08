7cf644a7deaefdd58b68f2599b40c552d9d04d05
Jay <jayzawrotny@gmail.com>
2021-08-07 20:36:53 -0400

    Converted tmux into a literate config

---
+++ tmux/install.sh
@@ -0,0 +1,11 @@
+# [[file:tmux.org::*Shebang][Shebang:1]]
+#!/usr/bin/env bash
+# Shebang:1 ends here
+
+# [[file:tmux.org::*Installer][Installer:1]]
+brew install tmux
+# Installer:1 ends here
+
+# [[file:tmux.org::*Installer][Installer:2]]
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Installer:2 ends here
